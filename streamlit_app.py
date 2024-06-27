5bdd7313e35160b467960ddd777a4680381e63f0
GPAVA811 <pavani.g2@cognizant.com>
2024-06-27 12:38:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,8 +33,8 @@ ingredients_list = st.multiselect(
 if ingredients_list:
      ingredients_string = ''
     
-     for fruit_choosen in ingredients_list:
-         ingredients_string += fruit_choosen + ' '
+     for fruit_chosen in ingredients_list:
+         ingredients_string += fruit_chosen + ' '
 
          search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'search_on'].iloc[0]
          st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
@@ -42,7 +42,7 @@ if ingredients_list:
 
 
          
-         st.subheader(fruit_choosen + 'Nutrition Information')
+         st.subheader(fruit_chosen + 'Nutrition Information')
          fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_chosen)
          fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
  
